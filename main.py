3abc9dd9fac85e204f98f55e8390d1eac181e786
Nathan Lauffer <purecognition@gmail.coml>
2019-11-03 20:34:02 -0500

    Added main.py and configuration file for Thaddeus for The Star.

---
+++ main.py
@@ -0,0 +1,50 @@
+import pprint
+import json
+import pyttsx3
+pp = pprint.PrettyPrinter(indent=4)
+engine = pyttsx3.init()
+
+with open('thestar-thaddeus.json') as json_file:
+    data = json.load(json_file)
+
+title = data['title']
+actor = data['actor']
+character = data['character']
+
+welcome_text = f'Welcome to {title} with {actor} as {character}'
+
+print(welcome_text)
+engine.say(welcome_text)
+engine.runAndWait()
+
+for scene in data['scenes']:
+    scene_number = scene['number']
+    scene_title = scene['title']
+
+    scene_text = f'Scene {scene_number}: {scene_title}'
+    print(scene_text)
+    engine.say(scene_text)
+    engine.runAndWait()
+
+    for part in sorted(scene['parts'], key=lambda x: x['number']):
+        part_number = part['number']
+
+        for line in sorted(part['lines'], key=lambda x: x['number']):
+            line_number = line['number']
+            line_character = ""
+            my_character = True
+            if "character" in line:
+                line_character = line['character']
+                my_character = False
+            else:
+                line_character = f'{character}'
+            line_content = line['content']
+
+            if my_character:
+                print(f'*{line_character}: {line_content}')
+                print()
+                input("Say line and then press Enter")
+            else:
+                print(f'{line_character}: {line_content}')
+                engine.say(line_content)
+                engine.runAndWait()
